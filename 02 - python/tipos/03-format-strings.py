c40e5a4db9d90e65799ddeb82d59002b6a581664
aldlt <alejandro.de.la.torre@hotmail.es>
2025-02-20 12:34:33 +0100

    Modificacion en format

--- 02 - python/tipos/03-format-strings.py
+++ 02 - python/tipos/03-format-strings.py
@@ -1,4 +1,5 @@
 nombre = "Alejandro"
 apellido = "de la Torre"
-nombre_completo = f"{nombre[0]} {2 + 5}"
+# nombre_completo = f"{nombre[0]} {2 + 5}"
+nombre_completo = f"{nombre} {apellido}"
 print(nombre_completo)
